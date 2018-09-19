0f68d4770b68f68ce006b66215a3842bddd12d89
Clay Griffiths <clay@claygriffiths.com>
2018-09-19 09:42:59 -0500

    feat(DX): add Webpack to handle CSS Modules and HMR
    * Remove custom wrapper component and react-shadow
    * feat(DX): add Webpack to handle CSS Modules and HMR
    * fix(formatting): reformat .babelrc
    
    LD-782

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,31 +1,21 @@
 const webpack = require('webpack');
 const path = require('path');
+const merge = require('webpack-merge');
 
 module.exports = {
     skipComponentsWithoutExample: true,
     getComponentPathLine(componentPath) {
         return `import { ${path.basename(componentPath, '.jsx')} } from 'local-components';`
     },
-    styleguideComponents: {
-        Wrapper: path.resolve(__dirname, 'styleguide', 'components', 'Wrapper'),
-    },
     components: 'src/**/[A-Z]*.jsx',
     webpackConfig: () => {
-        return {
-            module: {
-                rules: [
-                    {
-                        test: /\.jsx?$/,
-                        exclude: /node_modules/,
-                        loader: 'babel-loader'
-                    },
-                ],
-            },
-            plugins: [
-                new webpack.NormalModuleReplacementPlugin(/electron/, function (resource) {
-                    resource.request = path.resolve(__dirname, 'styleguide', 'stubs', 'electron.stub.js');
-                })
-            ]
-        };
+        return merge(require('./webpack.common'), {
+			target: 'web',
+			plugins: [
+				new webpack.NormalModuleReplacementPlugin(/electron/, function (resource) {
+					resource.request = path.resolve(__dirname, 'styleguide', 'stubs', 'electron.stub.js');
+				}),
+			]
+		});
     }
 };
