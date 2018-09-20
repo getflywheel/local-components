ec75f8ea8206bbc128e1c7af3477bc0842a35a30
Clay Griffiths <clay@claygriffiths.com>
2018-09-20 13:35:02 -0500

    refactor: replaced react-router v3 with react-router v4

--- src/VerticalNav/VerticalNav.jsx
+++ src/VerticalNav/VerticalNav.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { NavLink } from 'react-router-dom';
 import LocalSitesSVG from '../../svg/sites_local.svg';
 import ConnectNavSVG from '../../svg/connect-nav.svg';
 import AddSVG from '../../svg/add.svg';
@@ -17,12 +17,12 @@ export default class VerticalNav extends Component {
 		const localSitesActive = this.props.location.pathname.indexOf('/main') === 0 && this.props.location.pathname.indexOf('/main/flywheel') !== 0;
 
 		return <FlyTooltip content="Local Sites" position="right" hoverIntent={true}>
-			<Link to={linkLocation} className={classnames('VerticalNav_Local', {
+			<NavLink to={linkLocation} className={classnames('VerticalNav_Local', {
 				'--FadeIn': this.props.fadeIn,
 				'--Active': localSitesActive,
 			})}>
 				<LocalSitesSVG/>
-			</Link>
+			</NavLink>
 		</FlyTooltip>;
 
 	}
@@ -30,11 +30,11 @@ export default class VerticalNav extends Component {
 	renderFlywheelSitesLink () {
 
 		return <FlyTooltip content="Connect to Flywheel" position="right" hoverIntent={true}>
-			<Link to="/main/flywheel"
-			      className={classnames('VerticalNav_Flywheel', { '--FadeIn': this.props.fadeIn })}
-			      activeClassName="--Active">
+			<NavLink to="/main/flywheel"
+					 className={classnames('VerticalNav_Flywheel', { '--FadeIn': this.props.fadeIn })}
+					 activeClassName="--Active">
 				<ConnectNavSVG/>
-			</Link>
+			</NavLink>
 		</FlyTooltip>;
 
 	}
@@ -49,9 +49,9 @@ export default class VerticalNav extends Component {
 				<div className="DragRegion"/>
 
 				<FlyTooltip content="Add Local Site" position="right" hoverIntent={true} className="--AddSite">
-					<Link to="/main/add-site" className={classnames('VerticalNav_Add', { '--FadeIn': this.props.fadeIn })}>
+					<NavLink to="/main/add-site" className={classnames('VerticalNav_Add', { '--FadeIn': this.props.fadeIn })}>
 						<AddSVG />
-					</Link>
+					</NavLink>
 				</FlyTooltip>
 			</div>
 		);
