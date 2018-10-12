a4f7853e321891a4195bfe63d77823f71468c40c
Crum <chris.crum@getflywheel.com>
2018-10-11 21:08:11 -0500

    refactor: migrated FlyModal and Close to new CSS modules system with new Example

--- src/components/Close/Close.jsx
+++ src/components/Close/Close.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import CloseBigSVG from '../../svg/close--big.svg';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import CloseBigSVG from '../../svg/close--big.svg';
 import styles from './Close.sass';
 
 export default class Close extends Component {
@@ -12,7 +13,10 @@ export default class Close extends Component {
 	render () {
 		return (
 			<span
-				className={styles.Close}
+				className={classnames(
+					styles.Close,
+					'Close', // this also needs to be globally accessible so other component styles can reference it
+				)}
 				onClick={this.props.onClick}
 			>
 				<CloseBigSVG />
--- src/components/FlyModal/FlyModal.jsx
+++ src/components/FlyModal/FlyModal.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import classnames from 'classnames';
 import ReactModal from 'react-modal';
 import CloseSVG from '../../svg/close--big.svg';
+import Close from '../Close';
+import styles from './FlyModal.sass';
 
-ReactModal.setAppElement('#root');
+/**
+ * Try catch for Local vs. Styleguidist
+ */
+let ReactDOM;
+
+try {
+	ReactDOM = __non_webpack_require__('react-dom');
+} catch (e) {
+	ReactDOM = require('react-dom');
+}
+
+if(document.getElementById('root')) {
+	ReactModal.setAppElement('#root');
+}
+else {
+	ReactModal.setAppElement('body');
+}
 
 export default class FlyModal extends Component {
 
 	static defaultProps = {
+		hasTopIcon: false,
 		isOpen: true,
 		portalClassName: 'ReactModalPortal',
-		className: 'FlyModal',
-		overlayClassName: 'FlyModalOverlay',
+		overlayClassName: styles.FlyModalOverlay,
 		ariaHideApp: true,
 		closeTimeoutMS: 0,
 		shouldCloseOnOverlayClick: true,
@@ -20,15 +38,24 @@ export default class FlyModal extends Component {
 	};
 
 	static onRequestClose () {
-		document.getElementsByClassName('FlyModalOverlay')[0].classList.add('--FadeOut');
+		document.getElementsByClassName(styles.FlyModalOverlay)[0].classList.add('__FadeOut');
 
+		ReactDOM.unmountComponentAtNode(document.getElementById('popup-container'))
 		/* setTimeout used to wait for animation and to hack around "React DOM tree root should always have a node reference." warning */
-		setTimeout(() => ReactDOM.unmountComponentAtNode(document.getElementById('popup-container')), 210);
+		//setTimeout(() => ReactDOM.unmountComponentAtNode(document.getElementById('popup-container')), 210);
 	}
 
 	render () {
-		return <ReactModal {...this.props}>
-			<span className="Close" onClick={FlyModal.onRequestClose}><CloseSVG /></span>
+		return <ReactModal
+			{...this.props}
+			className={classnames(styles.FlyModal)} // warning: this must be set after {...this.props} to work
+		>
+			<Close
+				className={classnames({
+					[styles.FlyModal__HasIcon]: this.props.hasTopIcon
+				})}
+				onClick={FlyModal.onRequestClose}
+			/>
 			{this.props.children}
 		</ReactModal>;
 	}
