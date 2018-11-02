6c622b745a67861046c05bce27467764d0f05a13
Crum <chris.crum@getflywheel.com>
2018-11-02 09:16:18 -0500

    fix: tests now mock CSS Modules

--- src/components/DownloaderItem/DownloaderItem.test.jsx
+++ src/components/DownloaderItem/DownloaderItem.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import DownloaderItem from './DownloaderItem';
 
-jest.mock('electron', () => require('../../styleguide/stubs/electron.stub'), { virtual: true });
+jest.mock('electron', () => require('../../../styleguide/stubs/electron.stub'), { virtual: true });
 
 it('renders without crashing', () => {
 	shallow(<DownloaderItem />);
