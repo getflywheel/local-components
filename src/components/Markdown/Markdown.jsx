51e0d202c66d73ba27876346184213396898208d
Crum <chris.crum@getflywheel.com>
2018-11-09 09:04:46 -0600

    refactor: moved MarkdownCodeBlock class
    moved MarkdownCodeBlock class to within Markdown component so it doens’t appear as a separate styleguidist component

--- src/components/Markdown/Markdown.jsx
+++ src/components/Markdown/Markdown.jsx
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Markdown.sass';
 import ReactMarkdown from 'react-markdown';
-import MarkdownCodeBlock from './MarkdownCodeBlock';
+import apache from 'highlight.js/lib/languages/apache';
+import bash from 'highlight.js/lib/languages/bash';
+import css from 'highlight.js/lib/languages/css';
+import js from 'highlight.js/lib/languages/javascript';
+import json from 'highlight.js/lib/languages/json';
+import nginx from 'highlight.js/lib/languages/nginx';
+import php from 'highlight.js/lib/languages/php';
+import shell from 'highlight.js/lib/languages/shell';
+import sql from 'highlight.js/lib/languages/sql';
+import typescript from 'highlight.js/lib/languages/typescript';
+import xml from 'highlight.js/lib/languages/xml';
+import Lowlight from 'react-lowlight';
+import shallowCompare from 'react-addons-shallow-compare';
 
 export default class Markdown extends Component {
 
@@ -20,6 +32,54 @@ export default class Markdown extends Component {
 				renderers={{
 					code: MarkdownCodeBlock
 				}}
+			/>
+		);
+	}
+
+}
+
+class MarkdownCodeBlock extends Component {
+
+	static propTypes = {
+		value: PropTypes.string,
+		language: PropTypes.string,
+		inline: PropTypes.bool,
+	};
+
+	languagesEnabled = {
+		'apache': apache,
+		'bash': bash,
+		'css': css,
+		'javascript': js,
+		'json': json,
+		'nginx': nginx,
+		'php': php,
+		'shell': shell,
+		'sql': sql,
+		'typescript': typescript,
+		'xml': xml,
+	};
+
+	constructor (props) {
+		super(props);
+
+		for(const langName in this.languagesEnabled) {
+			Lowlight.registerLanguage(langName, this.languagesEnabled[langName]);
+		}
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return shallowCompare(this, nextProps, nextState);
+	};
+
+	render () {
+
+		return (
+			<Lowlight
+				className={styles.Lowlight}
+				language={this.props.language}
+				value={this.props.value}
+				inline={this.props.inline}
 			/>
 		);
 	}
