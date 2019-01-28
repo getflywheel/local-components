d3bfd275ddf9f47bb44d7ffe6920733295af8709
Clay Griffiths <clay@claygriffiths.com>
2019-01-28 10:09:17 -0600

    build: remove react-hot-loader

--- webpack.common.js
+++ webpack.common.js
@@ -20,7 +20,7 @@ module.exports = {
 	target: 'electron-renderer',
 	externals: [nodeExternals({
 		modulesDir: './node_modules',
-		whitelist: ['react-hot-loader', /webpack-dev-server/, /webpack\/hot/],
+		whitelist: [/webpack-dev-server/, /webpack\/hot/],
 	})],
 	plugins: [
 		new DtsBundleWebpack({
