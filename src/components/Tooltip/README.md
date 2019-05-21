5418aad96848a3084467687621957db564fa0c13
Crummm <clcrum@gmail.com>
2019-05-21 10:00:59 -0500

    improvement: add Tooltip position and forceHover

--- src/components/Tooltip/README.md
+++ src/components/Tooltip/README.md
@@ -1,7 +1,35 @@
+Default tooltip with `auto` position.
+
 ```js
-<div style={{ height: '80px' }}>
+<div style={{ height: '200px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
     <Tooltip
     	content={<button className="__Pill __Green __Medium">Hover over me</button>}
+    >
+    	This is a tooltip
+    </Tooltip>
+</div>
+```
+
+Position set to `right`.
+
+```js
+<div style={{ height: '200px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <Tooltip
+    	content={<button className="__Pill __Green __Medium">Hover over me</button>}
+    	position="right"
+    >
+    	This is a tooltip
+    </Tooltip>
+</div>
+```
+
+Set `forceHover` to true
+
+```js
+<div style={{ height: '200px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <Tooltip
+    	content={<button className="__Pill __Green __Medium">Hover over me</button>}
+    	forceHover={true}
     >
     	This is a tooltip
     </Tooltip>
--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -3,16 +3,18 @@ import IReactComponentProps from '../../common/structures/IReactComponentProps';
 import classnames from 'classnames';
 import * as styles from './Tooltip.scss';
 import { Manager, Reference, Popper } from 'react-popper';
-import { ResizeObserverComponent } from './ResizeObserverComponent';
 
 interface IProps extends IReactComponentProps {
 	content?: React.ReactElement;
+	forceHover?: boolean;
+	position?: 'auto' | 'auto-start' | 'auto-end' | 'top' | 'top-start' | 'top-end' | 'right' | 'right-start' | 'right-end' | 'bottom' | 'bottom-start' | 'bottom-end' | 'left' | 'left-start' | 'left-end';
 }
 
 export class Tooltip extends React.Component<IProps> {
 
 	static defaultProps: Partial<IProps> = {
-
+		forceHover: false,
+		position: 'auto',
 	};
 
 	render () {
@@ -23,18 +25,36 @@ export class Tooltip extends React.Component<IProps> {
 						if(!this.props.content) {
 							return null;
 						}
-						return (
-							<ResizeObserverComponent>
-								{React.cloneElement(this.props.content, {ref})}
-							</ResizeObserverComponent>
-						)
+						return React.cloneElement(
+							this.props.content,
+							{
+								ref,
+								className: classnames(
+									this.props.content.props.className,
+									styles.Tooltip_Content,
+								),
+							}
+						);
 					}}
 				</Reference>
-				<Popper placement="bottom">
+				<Popper placement={this.props.position}>
 					{({ ref, style, placement, arrowProps }) => (
-						<div ref={ref} style={style} data-placement={placement}>
+						<div
+							ref={ref}
+							className={classnames(
+								styles.Tooltip_Popper,
+								{
+									[styles.Tooltip_Popper__ForceHover]: this.props.forceHover,
+								}
+							)}
+							style={style}
+							data-placement={placement}
+						>
 							{this.props.children}
-							<div ref={arrowProps.ref} style={arrowProps.style} />
+							<div
+								ref={arrowProps.ref}
+								style={arrowProps.style}
+							/>
 						</div>
 					)}
 				</Popper>
