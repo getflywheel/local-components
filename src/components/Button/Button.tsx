2a8b4a4be93ea42fdd289710f2a49350cb80aa84
flycrum <crum@getflywheel.com>
2019-08-26 09:02:18 -0500

    feat: setup prop-based Button and remove base global button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,81 @@
 import * as React from 'react';
-import IReactComponentProps from '../../common/structures/IReactComponentProps';
+import classnames from 'classnames';
+import * as styles from './Button.scss';
+import { Container } from '../Container';
+import ILocalContainerProps from '../../common/structures/ILocalContainerProps';
+import Handler from '../../common/structures/Handler';
 
-interface IProps extends IReactComponentProps {
-
-	disabled?: boolean;
-
+export enum ButtonPropEmphasis {
+	primary = 'primary',
+	secondary = 'secondary',
+	text = 'text',
 }
 
+export enum ButtonPropSize {
+	s = 's',
+	m = 'm',
+	l = 'l',
+}
+
+export enum ButtonPropIntent {
+	danger = 'danger',
+	none = 'none',
+}
+
+interface IProps extends ILocalContainerProps {
+	disabled?: boolean;
+	/** The hierarchical importance of the button within the given context (e.g. only one primary button should exist at any given time). */
+	emphasis?: ButtonPropEmphasis | keyof typeof ButtonPropEmphasis; // to allow string literal for enum, the key must match the value and 'keyof typeof' used
+	/** The purpose of the buttons which then modifies the styles of the given `emphasis` in the appropriate ways. */
+	intent?: ButtonPropIntent | keyof typeof ButtonPropIntent;
+	onClick?: Handler;
+	/** The size of the button that may result in height, width, and font-size changes. */
+	size?: ButtonPropSize | keyof typeof ButtonPropSize;
+	/** The html element tag used for the button. */
+	tag?: string;
+	/** The default behavior of the button. */
+	type?: 'button' | 'submit' | 'reset';
+}
+
+/* todo - crum: stateless function */
 export default class Button extends React.Component<IProps> {
 
+	static defaultProps: Partial<IProps> = {
+		disabled: false,
+		emphasis: ButtonPropEmphasis.secondary,
+		intent: ButtonPropIntent.none,
+		size: ButtonPropSize.m,
+		tag: 'button',
+	};
+
 	render () {
+		const Tag: any = this.props.tag;
+
 		return (
-			<button
-				disabled={this.props.disabled}
-				{...this.props}
-			>
-				{this.props.children}
-			</button>
+			<Container {...this.props.container}>
+				<Tag
+					className={classnames(
+						styles.Button,
+						'Button',
+						this.props.className,
+						{
+							[styles.Button__Emphasis_Primary]: this.props.emphasis === ButtonPropEmphasis.primary,
+							[styles.Button__Emphasis_Secondary]: this.props.emphasis === ButtonPropEmphasis.secondary,
+							[styles.Button__Emphasis_Text]: this.props.emphasis === ButtonPropEmphasis.text,
+							[styles.Button__Intent_Danger]: this.props.intent === ButtonPropIntent.danger,
+							[styles.Button__Size_Large]: this.props.size === ButtonPropSize.l,
+							[styles.Button__Size_Medium]: this.props.size === ButtonPropSize.m,
+							[styles.Button__Size_Small]: this.props.size === ButtonPropSize.s,
+						},
+					)}
+					disabled={this.props.disabled}
+					onClick={this.props.onClick}
+					type={this.props.type}
+					{...this.props}
+				>
+					{this.props.children}
+				</Tag>
+			</Container>
 		);
 	}
 
