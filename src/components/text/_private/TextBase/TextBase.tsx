ffa572b60c008e0c8db75b5920de33fad0ede592
flycrum <crum@getflywheel.com>
2019-08-30 14:57:00 -0500

    fix: correct text sizes, colors, and add captions for Text component

--- src/components/text/_private/TextBase/TextBase.tsx
+++ src/components/text/_private/TextBase/TextBase.tsx
@@ -5,8 +5,8 @@ import ILocalContainerProps from '../../../../common/structures/ILocalContainerP
 import { Container } from '../../../Container';
 
 export enum TextBasePropColor {
-	gray_gray15_text = 'gray_gray15_text',
 	gray_gray75_title = 'gray_gray75_title',
+	graydark_gray15_text = 'graydark_gray15_text',
 	graydark_white_caption = 'graydark_white_caption',
 }
 export enum TextBasePropFontSize {
@@ -17,11 +17,9 @@ export enum TextBasePropFontSize {
 }
 
 export enum TextBasePropFontWeight {
-	light = 'light',
 	normal = 'normal',
 	medium = 'medium',
 	bold = 'bold',
-	heavy = 'heavy',
 }
 
 export interface ITextCommonProps extends ILocalContainerProps {
@@ -43,7 +41,7 @@ export class TextBase extends React.Component<ITextBaseProps> {
 	static defaultProps: Partial<ITextBaseProps> = {
 		color: TextBasePropColor.graydark_white_caption,
 		fontSize: TextBasePropFontSize.s,
-		fontWeight: TextBasePropFontWeight.light,
+		fontWeight: TextBasePropFontWeight.medium,
 		tag: 'span',
 	};
 
@@ -59,18 +57,16 @@ export class TextBase extends React.Component<ITextBaseProps> {
 						'TextBase',
 						className,
 						{
-							[styles.TextBase__Color_Gray_Gray15]: color === TextBasePropColor.gray_gray15_text,
 							[styles.TextBase__Color_Gray_Gray75]: color === TextBasePropColor.gray_gray75_title,
+							[styles.TextBase__Color_GrayDark_Gray15]: color === TextBasePropColor.graydark_gray15_text,
 							[styles.TextBase__Color_GrayDark_White]: color === TextBasePropColor.graydark_white_caption,
 							[styles.TextBase__FontSize_Small]: fontSize === TextBasePropFontSize.s,
 							[styles.TextBase__FontSize_Medium]: fontSize === TextBasePropFontSize.m,
 							[styles.TextBase__FontSize_Large]: fontSize === TextBasePropFontSize.l,
 							[styles.TextBase__FontSize_XLarge]: fontSize === TextBasePropFontSize.xl,
-							[styles.TextBase__FontWeight_300Light]: fontWeight === TextBasePropFontWeight.light,
-							[styles.TextBase__FontWeight_400Normal]: fontWeight === TextBasePropFontWeight.normal,
-							[styles.TextBase__FontWeight_500Medium]: fontWeight === TextBasePropFontWeight.medium,
-							[styles.TextBase__FontWeight_700Bold]: fontWeight === TextBasePropFontWeight.bold,
-							[styles.TextBase__FontWeight_900Heavy]: fontWeight === TextBasePropFontWeight.heavy,
+							[styles.TextBase__FontWeight_300]: fontWeight === TextBasePropFontWeight.normal,
+							[styles.TextBase__FontWeight_500]: fontWeight === TextBasePropFontWeight.medium,
+							[styles.TextBase__FontWeight_700]: fontWeight === TextBasePropFontWeight.bold,
 						},
 					)}
 					{...otherProps}
