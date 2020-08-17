442caf08c23f87343124fce9729c418bd2e4ce5b
veryspry <ehlinger.matt@gmail.com>
2020-08-17 14:12:26 -0500

    refactor: convert SiteInfoInnerPane from class to function

--- src/components/modules/SiteInfoInnerPane/SiteInfoInnerPane.tsx
+++ src/components/modules/SiteInfoInnerPane/SiteInfoInnerPane.tsx
@@ -2,18 +2,15 @@ import * as React from 'react';
 import IReactComponentProps from '../../../common/structures/IReactComponentProps';
 import * as styles from './SiteInfoInnerPane.sass';
 
-export default class SiteInfoInnerPane extends React.Component<IReactComponentProps> {
-	render () {
-		const propsWithoutChildren = { ...this.props };
-		delete propsWithoutChildren.children;
+export default function SiteInfoInnerPane(props: IReactComponentProps) {
+	const { children, ...propsWithoutChildren } = props;
 
-		return (
-			<div
-				className={styles.SiteInfoInnerPane}
-				{...propsWithoutChildren}
-			>
-				{this.props.children}
-			</div>
-		);
-	}
+	return (
+		<div
+			className={styles.SiteInfoInnerPane}
+			{...propsWithoutChildren}
+		>
+			{children}
+		</div>
+	);
 }
