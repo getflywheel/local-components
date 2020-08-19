b7abeb6d8fd357eb93dd4520374e2570eca8ca29
Clay Griffiths <clay@claygriffiths.com>
2020-08-19 16:33:17 -0500

    docs: wrap all previews with <MemoryRouter />

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 import '../src/global.sass';
+import React from 'react';
+import { MemoryRouter } from 'react-router';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -7,3 +9,20 @@ export const parameters = {
     { name: 'Dark Mode', class: 'Theme__Dark', color: '#363637' },
   ],
 }
+
+/**
+ * Some of our stories depend on react-router in which case we need a basic React Router
+ * booted up.
+ * 
+ * @param {*} Story 
+ * @param {*} context 
+ */
+const withMemoryRouter=(Story, context)=>{
+  return (
+    <MemoryRouter>
+      <Story {...context} />
+    </MemoryRouter>
+  )
+}
+
+export const decorators = [withMemoryRouter];
