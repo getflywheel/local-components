9dfdfa050a21aca2abe3673305f7243f23ec63a6
Alexa <alexa.sparks@wpengine.com>
2020-09-16 07:18:28 -0500

    feat(LOC-2111): add IconCheckbox component & star svgs & unit tests

---
+++ src/components/inputs/IconCheckbox/IconCheckbox.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import classnames from 'classnames';
+import IReactComponentProps from '../../../common/structures/IReactComponentProps';
+import * as styles from './IconCheckbox.scss';
+import { FunctionGeneric } from '../../../common/structures/Generics';
+
+interface ISvgProps {
+	className?: string;
+}
+
+interface IProps extends IReactComponentProps {
+	checked?: boolean;
+	disabled?: boolean;
+	onChange: FunctionGeneric;
+	svgCheckedIcon: React.FC<ISvgProps>;
+	svgUncheckedIcon: React.FC<ISvgProps>;
+}
+
+export const IconCheckbox: React.FC<IProps> = ({
+	checked,
+	disabled,
+	onChange,
+	svgCheckedIcon,
+	svgUncheckedIcon,
+	...props
+}) => {
+	const [ isChecked, updateCheckedState ] = useState(checked === undefined ? false : checked);
+
+	const handleChange = () => {
+		const checkedUpdate: boolean = !isChecked;
+
+		updateCheckedState(checkedUpdate);
+
+		if (onChange) {
+			onChange(isChecked);
+		}
+	};
+
+	const CheckedSVG = svgCheckedIcon;
+	const UncheckedSVG = svgUncheckedIcon;
+
+	return (
+		<div
+			className={classnames(
+				styles.IconCheckbox,
+				{
+					[styles.IconCheckbox__Checked]: isChecked === true,
+					[styles.IconCheckbox__Disabled]: disabled,
+				},
+				props.className,
+			)}
+			id={props.id}
+			style={props.style}
+		>
+			<input
+				type="checkbox"
+				className={styles.IconCheckbox_InputHidden}
+				checked={isChecked === true}
+				disabled={disabled}
+				onChange={handleChange}
+			/>
+			{isChecked && <CheckedSVG className={styles.IconCheckbox_Icon} />}
+			{!isChecked && <UncheckedSVG className={styles.IconCheckbox_Icon} />}
+		</div>
+	);
+};
+
+export default IconCheckbox;
