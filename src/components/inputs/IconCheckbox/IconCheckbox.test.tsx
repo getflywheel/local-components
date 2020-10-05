7e8701311fb037cd3ebf01245d381bb4954286e9
flycrum <41925404+flycrum@users.noreply.github.com>
2020-10-05 11:36:54 -0500

    feat(LOC-2115): rewrite IconCheckbox to use 1 svg, allow color overrides, scale on active, and fix hover states

--- src/components/inputs/IconCheckbox/IconCheckbox.test.tsx
+++ src/components/inputs/IconCheckbox/IconCheckbox.test.tsx
@@ -2,17 +2,16 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import IconCheckbox from './IconCheckbox';
 import YellowStar from '../../../svg/star--yellow.svg';
-import GrayStar from '../../../svg/star--gray.svg';
 
 import TestComponentPropUtils from '../../../utils/TestComponentPropUtils';
 
 describe('Checkbox', () => {
 	it('renders without crashing', () => {
-		shallow(<IconCheckbox onChange={() => null} svgCheckedIcon={YellowStar} svgUncheckedIcon={GrayStar} />);
+		shallow(<IconCheckbox onChange={() => null} Icon={YellowStar} />);
 	});
 
 	it('renders basic react props like id, className, and style as element attributes', () => {
-		const shallowWrapper = shallow(<IconCheckbox onChange={() => null} svgCheckedIcon={YellowStar} svgUncheckedIcon={GrayStar} {...TestComponentPropUtils.basicReactProps} />);
+		const shallowWrapper = shallow(<IconCheckbox onChange={() => null} Icon={YellowStar} {...TestComponentPropUtils.basicReactProps} />);
 		TestComponentPropUtils.expectsBasicReactProps(shallowWrapper, false);
 	});
 });
