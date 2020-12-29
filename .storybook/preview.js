3e96ee4403dc3f805e50784fd8a49e5d97eb36bb
flycrum <41925404+flycrum@users.noreply.github.com>
2020-12-29 16:19:01 -0600

    dx: simplify storybook theme view options and fix scrolling issues

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -52,66 +52,62 @@ const withMemoryRouter = (Story, context) => {
 
 	return (
 		<MemoryRouter>
-			{theme === 'light' || theme === 'dark'
-				? (
-					{/* examples that depend on an outer absolute position need this to appear correctly within the story */}
-					<div
-						className={theme === 'light' ? 'Theme__Light' : 'Theme__Dark'}
-						style={{position: 'relative'}}
-					>
-						<Story {...context} />
-					</div>
-				)
-				: (
-					<div
-						style={{
-							display: 'flex',
-							height: '100%',
-							['flexDirection']: theme === 'stacked' ? 'column' : 'row',
-						}}
-					>
+			<div
+				style={{
+					display: 'flex',
+					position: 'absolute',
+					top: '0',
+					right: '0',
+					bottom: '0',
+					left: '0',
+					flexDirection: theme === 'stacked'? 'column' : 'row',
+				}}
+			>
+				{
+					[
+						// use light mode for everything except dark mode (including stacked and split)
+						theme !== 'dark' && {
+							themeMode: 'Theme__Light',
+							bgColor: '#FFFFFF',
+							textColor: '#262727',
+							header: 'Light Mode',
+						},
+						// use dark mode for everything except dark mode (including stacked and split)
+						theme !== 'light' && {
+							themeMode: 'Theme__Dark',
+							bgColor: '#262727',
+							textColor: '#FFFFFF',
+							header: 'Dark Mode',
+						},
+					// loop over modes to render depending on whether split, stacked, light, or dark
+					].filter(Boolean).map((themeData) => (
 						<div
-							className="Theme__Light"
+							key={themeData.themeMode}
+							className={themeData.themeMode}
 							style={{
-								background: '#FFFFFF',
-								flex: '0 0 50%',
-								padding: '10px',
-								position: 'relative',
+								background: themeData.bgColor,
+								display: 'flex',
+								flexDirection: 'column',
+								flex: theme === 'stacked' || theme === 'split' ? '0 0 50%' : '1',
+								minHeight: theme === 'stacked'? '0' : 'initial', // silly flexbox fix
 							}}
 						>
 							<h1
 								style={{
-									marginBottom: '10px',
-									textDecoration: 'underline',
+									color: themeData.textColor,
+									margin: '10px',
 								}}
 							>
-								Light Mode
+								{ themeData.header }
 							</h1>
-							<Story {...context} />
+							{/* examples that depend on an outer absolute position need this to appear correctly within the story */}
+							<div style={{position: 'relative', flexGrow: '1', overflow: 'auto'}}>
+								<Story {...context} />
+							</div>
 						</div>
-						<div
-							className="Theme__Dark"
-							style={{
-								background: '#262727',
-								flex: '0 0 50%',
-								padding: '10px',
-								position: 'relative',
-							}}
-						>
-							<h1
-								style={{
-									color: '#FFFFFF',
-									marginBottom: '10px',
-									textDecoration: 'underline',
-								}}
-							>
-								Dark Mode
-							</h1>
-							<Story {...context} />
-						</div>
-					</div>
-				)
-			}
+					))
+				}
+			</div>
 		</MemoryRouter>
 	)
 }
