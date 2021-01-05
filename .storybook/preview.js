c32d3e846572b10880758a0597a38bc5529a5552
flycrum <41925404+flycrum@users.noreply.github.com>
2021-01-05 14:57:48 -0600

    dx(LOC-2321): update storybook preview to support popper overlay container
    squash

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,26 +3,29 @@ import React from 'react';
 import { MemoryRouter } from 'react-router';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  themes: [
-    { name: 'Light Mode', class: 'Theme__Light', color: '#51bb7b', default: true },
-    { name: 'Dark Mode', class: 'Theme__Dark', color: '#363637' },
-  ],
+	actions: {argTypesRegex: "^on[A-Z].*"},
+	themes: [
+		{name: 'Light Mode', class: 'Theme__Light', color: '#51bb7b', default: true},
+		{name: 'Dark Mode', class: 'Theme__Dark', color: '#363637'},
+	],
 }
 
 /**
  * Some of our stories depend on react-router in which case we need a basic React Router
  * booted up.
- * 
- * @param {*} Story 
- * @param {*} context 
+ *
+ * @param {*} Story
+ * @param {*} context
  */
-const withMemoryRouter=(Story, context)=>{
-  return (
-    <MemoryRouter>
-      <Story {...context} />
-    </MemoryRouter>
-  )
+const withMemoryRouter = (Story, context) => {
+	return (
+		<MemoryRouter>
+			{/* examples that depend on an outer absolute position need this to appear correctly within the story */}
+			<div style={{position: 'relative'}}>
+				<Story {...context} />
+			</div>
+		</MemoryRouter>
+	)
 }
 
 export const decorators = [withMemoryRouter];
