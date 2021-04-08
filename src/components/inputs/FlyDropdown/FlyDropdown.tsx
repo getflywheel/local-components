3763a04512f9b3ae26314cf48cb4212bb1b6a77c
flycrum <41925404+flycrum@users.noreply.github.com>
2021-04-08 16:25:54 -0500

    refactor(LOC-193): convert FlyTooltip from class-based to functional component

--- src/components/inputs/FlyDropdown/FlyDropdown.tsx
+++ src/components/inputs/FlyDropdown/FlyDropdown.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState } from 'react';
 import classnames from 'classnames';
 import * as styles from './FlyDropdown.scss';
 import CaretSVG from '../../../svg/caret.svg';
@@ -20,106 +21,103 @@ interface IProps extends IReactComponentProps {
 	position?: 'top' | 'bottom';
 }
 
-interface IState {
-	open: boolean;
-}
+const FlyDropdown = (props: IProps) => {
+	const {
+		caret,
+		children,
+		className,
+		id,
+		items,
+		navItem,
+		navItemActive,
+		position,
+		style,
+	} = props;
+	const [ open, setOpen ] = useState(false);
 
-export default class FlyDropdown extends React.Component<IProps, IState> {
-	static defaultProps: Partial<IProps> = {
-		caret: true,
-		items: [],
-		navItem: false,
-		navItemActive: false,
-		position: 'bottom',
+	const onClick = () => {
+		setOpen((open) => !open);
 	};
 
-	constructor (props: IProps) {
-		super(props);
-
-		this.state = {
-			open: false,
-		};
-
-		this.onClick = this.onClick.bind(this);
-		this.onBlur = this.onBlur.bind(this);
+	const onBlur = () => {
+		setOpen(false);
 	}
 
-	onClick () {
-		this.setState({
-			open: !this.state.open,
-		});
+	const onClickItem = (event: React.MouseEvent<HTMLLIElement, MouseEvent>, item: IItems) => {
+		item.onClick.call(null);
+		setOpen(false);
+		event.stopPropagation();
 	}
 
-	onBlur () {
-		this.setState({
-			open: false,
-		});
-	}
-
-	render () {
-		return (
-			<div
-				className={classnames(
-					styles.FlyDropdown,
-					'FlyDropdown',
-					{
-						[styles.FlyDropdown__Open]: this.state.open,
-						'FlyDropdown__Open': this.state.open, // this also needs to be globally accessible so other component styles can reference it
-						[styles.FlyDropdown__NavItem]: this.props.navItem,
-						[styles.FlyDropdown__NavItemActive]: this.props.navItemActive,
-					},
-					this.props.className,
-				)}
-				id={this.props.id}
-				onClick={this.onClick}
-				onBlur={this.onBlur}
-				style={this.props.style}
-				tabIndex={0}
-			>
-				{this.props.children}
-				{this.props.caret && (
-					<CaretSVG
-						className={classnames(
-							styles.FlyDropdown_Caret,
-							'FlyDropdown_Caret'
-							)}
-						/>
-				)}
-				<ul
+	return (
+		<div
+			className={classnames(
+				styles.FlyDropdown,
+				'FlyDropdown',
+				{
+					[styles.FlyDropdown__Open]: open,
+					'FlyDropdown__Open': open, // this also needs to be globally accessible so other component styles can reference it
+					[styles.FlyDropdown__NavItem]: navItem,
+					[styles.FlyDropdown__NavItemActive]: navItemActive,
+				},
+				className,
+			)}
+			id={id}
+			onClick={onClick}
+			onBlur={onBlur}
+			style={style}
+			tabIndex={0}
+		>
+			{children}
+			{caret && (
+				<CaretSVG
 					className={classnames(
-						styles.FlyDropdown_Items,
-						'FlyDropdown_Items',
-						{
-							[styles.FlyDropdown_Items__PositionTop]: this.props.position === 'top',
-						},
+						styles.FlyDropdown_Caret,
+						'FlyDropdown_Caret'
 					)}
-				>
+				/>
+			)}
+			<ul
+				className={classnames(
+					styles.FlyDropdown_Items,
+					'FlyDropdown_Items',
 					{
-						this.props.items.map((item: IItems, i: number) => (
-							<li
-								key={i}
-								className={classnames(
-									styles.FlyDropdown_Item,
-									'FlyDropdown_Item',
-									{
-										[styles.FlyDropdown_Item__ColorNone]: item.color === 'none',
-										[styles.FlyDropdown_Item__ColorRed]: item.color === 'red',
-									}
-								)}
-								onClick={(event) => {
-									item.onClick.call(null);
-									this.setState({ open: false });
-									event.stopPropagation();
-								}}
-								onMouseDown={(event) => event.preventDefault()}
-							>
-								{item.label}
-								{item.content}
-							</li>
-						))
-					}
-				</ul>
-			</div>
-		);
-	}
+						[styles.FlyDropdown_Items__PositionTop]: position === 'top',
+					},
+				)}
+			>
+				{
+					items.map((item: IItems, i: number) => (
+						<li
+							key={i}
+							className={classnames(
+								styles.FlyDropdown_Item,
+								'FlyDropdown_Item',
+								{
+									[styles.FlyDropdown_Item__ColorNone]: item.color === 'none',
+									[styles.FlyDropdown_Item__ColorRed]: item.color === 'red',
+								}
+							)}
+							onClick={(event) => onClickItem(event, item)}
+							onMouseDown={(event) => event.preventDefault()}
+						>
+							{item.label}
+							{item.content}
+						</li>
+					))
+				}
+			</ul>
+		</div>
+	);
 }
+
+FlyDropdown.defaultProps = {
+	caret: true,
+	forceHover: true,
+	items: [],
+	navItem: false,
+	navItemActive: false,
+	position: 'bottom',
+} as Partial<IProps>;
+
+export default FlyDropdown;
