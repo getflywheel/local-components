77596ff92d58a27c5f9042a1ebbc86149f338068
Adam Perry <adam.perry@wpengine.com>
2021-09-15 13:21:47 -0500

    feat(LOC-2997): added CopyInput example and playground to Storybook

---
+++ src/components/inputs/CopyInput/examples/CopyInputPlayground.tsx
@@ -0,0 +1,46 @@
+
+import { ComponentExampleBase } from '../../../../common/helpers/ComponentExampleBase';
+import { CopyInput, ICopyInputProps } from '../CopyInput';
+
+
+export class CopyInputPlayground extends ComponentExampleBase {
+
+	constructor (props: ICopyInputProps) {
+		super(
+			props,
+			CopyInput,
+			'CopyInput',
+			[
+				{
+					propName: 'placeholder',
+					type: 'string',
+				},
+				{
+					propName: 'value',
+					type: 'string',
+				},
+				{
+					propName: 'label',
+					type: 'string',
+				},
+				{
+					propName: 'message',
+					type: 'string',
+				},
+				{
+					propName: 'onlyShowMessageWhenInvalid',
+					type: 'boolean',
+				},
+				{
+					propName: 'invalid',
+					type: 'boolean',
+				},
+                {
+					propName: 'disabled',
+					type: 'boolean',
+				},
+			],
+		);
+	}
+
+}
