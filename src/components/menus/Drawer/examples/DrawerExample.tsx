cd7baa4e49088a22b23a13d434da097713a9eddd
Adam Perry <adam.perry@wpengine.com>
2021-09-16 16:42:55 -0500

    feat(LOC-2998): updated drawer component to match design needs

--- src/components/menus/Drawer/examples/DrawerExample.tsx
+++ src/components/menus/Drawer/examples/DrawerExample.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Drawer from '../Drawer';
 import { Button } from '../../../buttons/Button/Button';
 import { PrimaryButton } from '../../../buttons/PrimaryButton/PrimaryButton';
+import { TextButton } from '../../../buttons/TextButton/TextButton';
 
 interface IState {
 	showDrawer: boolean;
 }
 
-export default class DrawerExample extends React.Component<{}, IState>{
+export class DrawerExample extends React.Component<{}, IState>{
 	constructor (props: {}, context: any) {
 		super(props, context);
 
@@ -23,6 +24,29 @@ export default class DrawerExample extends React.Component<{}, IState>{
 
 				<Drawer show={this.state.showDrawer}>
 					<PrimaryButton onClick={() => console.log('onClick')}>Save</PrimaryButton>
+				</Drawer>
+			</>
+		);
+	}
+}
+
+export class DrawerExample2 extends React.Component<{}, IState>{
+	constructor (props: {}, context: any) {
+		super(props, context);
+
+		this.state = {
+			showDrawer: false,
+		};
+	}
+
+	render () {
+		return (
+			<>
+				<Button onClick={() => this.setState({showDrawer: !this.state.showDrawer})}>{this.state.showDrawer ? 'Hide Drawer' : 'Show Drawer'}</Button>
+
+				<Drawer show={this.state.showDrawer} stripes={false} shadow>
+					<PrimaryButton onClick={() => console.log('onClick')}  privateOptions={{padding: 'm'}}>Save Changes</PrimaryButton>
+					<TextButton onClick={() => console.log('onClick')}>Cancel</TextButton>
 				</Drawer>
 			</>
 		);
