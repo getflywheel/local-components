d04a9dd3beb1d5c8c7a9ee5a2e04c92180de4982
Adam Perry <adam.perry@wpengine.com>
2021-09-17 11:51:37 -0500

    feat(LOC-2999): add refresh icon to icons

---
+++ src/components/icons/svgs/RefreshIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Refresh from '../../../svg/refresh.svg';
+import withIconSvg from '../helpers/withIconSvg';
+
+export const RefreshIcon: React.FC = ({...props}) => {
+	return (
+		<Refresh {...props} />
+	);
+};
+
+export default withIconSvg(RefreshIcon, true, { tags: ['refresh']});
