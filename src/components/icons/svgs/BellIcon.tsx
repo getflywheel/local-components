70d0c83f4bf943e166e7056d7e82171c8d92045d
Adam Perry <adam.perry@wpengine.com>
2021-10-26 14:57:14 -0500

    feat: add support page icons

---
+++ src/components/icons/svgs/BellIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Bell from '../../../svg/bell.svg';
+import withIconSvg from '../helpers/withIconSvg';
+
+export const BellIcon: React.FC = ({...props}) => {
+	return (
+		<Bell {...props} />
+	);
+};
+
+export default withIconSvg(BellIcon, true, { tags: ['bell', 'request', 'notification']});
