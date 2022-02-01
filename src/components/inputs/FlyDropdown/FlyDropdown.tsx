31896a29b0f334255ef799ad324c4871faa88ad0
Adam Perry <adam.perry@wpengine.com>
2022-02-01 16:42:37 -0600

    feat: address PR comments

--- src/components/inputs/FlyDropdown/FlyDropdown.tsx
+++ src/components/inputs/FlyDropdown/FlyDropdown.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import classnames from 'classnames';
 import * as styles from './FlyDropdown.scss';
-// import CaretSVG from '../../../svg/caret.svg';
 import { CaretIcon, CheckmarkMixedIcon } from '../../icons/Icons';
 import IReactComponentProps from '../../../common/structures/IReactComponentProps';
 import { FunctionGeneric } from '../../../common/structures/Generics';
