d6bcf95025d721765e2bd0c3986a8dcd1c7d1f02
Adam Perry <adam.perry@wpengine.com>
2022-03-03 15:00:52 -0600

    test: add testing-library and ContextMenu test

--- src/components/inputs/ContextMenu/ContextMenu.test.tsx
+++ src/components/inputs/ContextMenu/ContextMenu.test.tsx
@@ -1,20 +1,69 @@
-// import * as React from 'react';
-// import { shallow } from 'enzyme';
-// import ContextMenu from './ContextMenu';
-// import TestComponentPropUtils from '../../../utils/TestComponentPropUtils';
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import ContextMenu, { IContextMenuProps, IMenuItem } from './ContextMenu';
 
-// describe('FlyDropdown', () => {
-// 	it('renders without crashing', () => {
-// 		shallow(<FlyDropdown items={[]} />);
-// 	});
+const mockOnClick = jest.fn();
 
-// 	it('renders basic react props like id, className, and style as element attributes', () => {
-// 		const shallowWrapper = shallow(
-// 			<FlyDropdown
-// 				items={[]}
-// 				{...TestComponentPropUtils.basicReactProps}
-// 			/>
-// 		);
-// 		TestComponentPropUtils.expectsBasicReactProps(shallowWrapper, false);
-// 	});
-// });
+const menuItems: IMenuItem[] = [
+    {
+        label: 'View site',
+        enabled: true,
+        onClick: () => {
+            mockOnClick();
+        },
+    },
+    {
+        label: 'Admin dashboard',
+        enabled: false,
+        onClick: () => {
+            mockOnClick();
+        },
+    },
+    {
+        label: 'Reveal in Finder',
+        onClick: () => {
+            mockOnClick();
+        },
+    },
+    {
+        label: 'Open site shell',
+        onClick: () => {
+            mockOnClick();
+        },
+    },
+];
+
+const renderContextMenu = (props: IContextMenuProps = { items: menuItems }) => {
+    return render(<ContextMenu items={props.items} />);
+};
+
+const openContextMenu = () => userEvent.click(screen.getByRole('button', { name: /open context menu/i }));
+
+describe('ContextMenu', () => {
+    afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    it('displays correctly', () => {
+        renderContextMenu();
+        expect(screen.getByRole('button', { name: /open context menu/i })).toBeInTheDocument();
+    });
+
+    it('displays the context menu correctly on click', async () => {
+        renderContextMenu();
+        openContextMenu();
+        expect(await screen.findAllByRole('button')).toHaveLength(5);
+        expect(screen.getByRole('button', { name: menuItems[0].label! })).toBeInTheDocument();
+        expect(screen.getByRole('button', { name: menuItems[1].label! })).toHaveAttribute('disabled');
+        expect(screen.getByRole('button', { name: menuItems[3].label! })).not.toHaveAttribute('disabled');
+    });
+
+    it('calls the click handlers of the menu items on click', async () => {
+        renderContextMenu();
+        openContextMenu();
+        expect(await screen.findAllByRole('button')).toHaveLength(5);
+        menuItems.forEach((item) => userEvent.click(screen.getByRole('button', { name: item.label })));
+		expect(mockOnClick).toHaveBeenCalledTimes(3);
+	});
+});
