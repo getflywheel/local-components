4a1b0ffa18fe9990a172f7b26a00ce2085105775
Adam Perry <adam.perry@wpengine.com>
2022-03-07 15:23:48 -0600

    feat: address PR comments and update button styles

--- src/components/menus/ContextMenu/ContextMenu.tsx
+++ src/components/menus/ContextMenu/ContextMenu.tsx
@@ -2,10 +2,9 @@ import * as React from 'react';
 import classnames from 'classnames';
 import * as styles from './ContextMenu.scss';
 import { DotsIcon } from '../../icons/Icons';
-import IReactComponentProps from '../../../common/structures/IReactComponentProps';
 import { FunctionGeneric } from '../../../common/structures/Generics';
 import { Tooltip, TooltipProps } from '../../overlays/Tooltip/Tooltip';
-import { PrimaryButton } from '../../buttons/PrimaryButton/PrimaryButton';
+import { Button } from '../../buttons/Button/Button';
 import { TextButton } from '../../buttons/TextButton/TextButton';
 
 export interface IMenuItem {
@@ -18,33 +17,17 @@ export interface IMenuItem {
     type?: 'normal' | 'separator';
 }
 
-export interface IContextMenuProps extends IReactComponentProps {
+export interface IContextMenuProps extends Omit<TooltipProps, 'content'> {
     /** className for the list items' container */
     classNameList?: string;
     /** className for an individual list item */
     classNameListItem?: string;
-    /** whether to force the tooltip to show and ignore mouse events **/
-    forceHover?: boolean;
     /** array of menu items */
     items: IMenuItem[];
-    popperOptions?: TooltipProps;
-    position?: 'top' | 'bottom';
-    useClickInsteadOfHover?: boolean;
 }
 
 const ContextMenu = (props: IContextMenuProps) => {
-    const {
-        className,
-        classNameList,
-        classNameListItem,
-        items,
-        popperOptions,
-        position,
-        useClickInsteadOfHover,
-        ...otherProps
-    } = props;
-
-    const { popperOffsetModifier, ...restPopperOptions } = popperOptions ?? {};
+    const { className, classNameList, classNameListItem, items, ...otherProps } = props;
 
     const onClickItem = (event: React.MouseEvent<HTMLLIElement, MouseEvent>, item: IMenuItem) => {
         item.onClick?.call(null);
@@ -55,62 +38,59 @@ const ContextMenu = (props: IContextMenuProps) => {
 
     const content = (
         <ul className={classnames(styles.ContextMenu_Items, 'ContextMenu_Items', classNameList)}>
-            {items.map((item: IMenuItem, i: number) => (
-                <li key={i} className={classnames(styles.ContextMenu_Item, classNameListItem, item.className)}>
-                    {item.type === 'separator' ? (
-                        <div style={{ flexGrow: 1, padding: '10px 0' }}></div>
-                    ) : (
-                        <TextButton
-                            className={item.color !== 'red' ? styles.ContextMenu_Item_TextButton : ''}
-                            onClick={(event) => onClickItem(event, item)}
-                            disabled={item.enabled === undefined ? false : !item.enabled}
-                            intent={item.color === 'red' ? 'destructive' : 'default'}
-                            style={{ flexGrow: 1, padding: '10px 0' }}
-                        >
-                            {item.label}
-                            {item.content}
-                        </TextButton>
-                    )}
-                </li>
-            ))}
+            {items
+                .filter((item: IMenuItem) => Object.keys(item).length !== 0)
+                .map((item: IMenuItem, i: number) => (
+                    <li key={i} className={classnames(styles.ContextMenu_Item, classNameListItem, item.className)}>
+                        {item.type === 'separator' ? (
+                            <div className={styles.ContextMenu_Divider}></div>
+                        ) : (
+                            <TextButton
+                                className={item.color !== 'red' ? styles.ContextMenu_Item_TextButton : ''}
+                                onClick={(event) => onClickItem(event, item)}
+                                disabled={item.enabled === undefined ? false : !item.enabled}
+                                intent={item.color === 'red' ? 'destructive' : 'default'}
+                                style={{ flexGrow: 1, padding: '5px 0', lineHeight: '20px' }}
+                                privateOptions={{ fontWeight: 'light' }}
+                            >
+                                {item.label}
+                                {item.content}
+                            </TextButton>
+                        )}
+                    </li>
+                ))}
         </ul>
     );
 
     return (
         <Tooltip
             className={classnames(styles.ContextMenu, className)}
-            content={content}
-            popperOffsetModifier={{
-                ...popperOffsetModifier,
-            }}
             popperVisualContainerClassName={styles.ContextMenu_PopperVisualContainer}
-            position={position === 'top' ? 'top-start' : 'bottom-start'}
-            hideDelay={useClickInsteadOfHover ? 0 : 300}
-            useClickInsteadOfHover={useClickInsteadOfHover}
-            showDelay={0}
-            hideArrow
+            hideDelay={props.useClickInsteadOfHover ? 0 : 300}
             onShow={() => setIsShowing(true)}
             onHide={() => setIsShowing(false)}
             {...otherProps}
-            {...restPopperOptions}
+            content={content}
         >
-            <PrimaryButton
-                aria-label='Open context menu'
+            <Button
+                aria-label="Open context menu"
                 active={isShowing}
                 className={styles.ContextMenu_Trigger}
                 privateOptions={{ style: { padding: '6px' } }}
             >
                 <DotsIcon />
-            </PrimaryButton>
+            </Button>
         </Tooltip>
     );
 };
 
 ContextMenu.defaultProps = {
-    forceHover: false,
+    forceShow: false,
     items: [],
-    position: 'bottom',
+    position: 'bottom-start',
     useClickInsteadOfHover: true,
+    hideArrow: true,
+    showDelay: 0,
 } as Partial<IContextMenuProps>;
 
 export default ContextMenu;
