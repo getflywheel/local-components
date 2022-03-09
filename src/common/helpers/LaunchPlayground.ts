66376db9de260037bdc9ee4e949b9f394b31d1de
Adam Perry <adam.perry@wpengine.com>
2022-03-09 13:53:29 -0600

    test: add tooltip tests

---
+++ src/common/helpers/LaunchPlayground.ts
@@ -0,0 +1,22 @@
+import { compressToEncodedURIComponent } from 'lz-string';
+import { execSync } from 'child_process';
+
+// Launches the playground feature from the JavaScript Testing Library mob as a
+// side-effect rather than printing that gnarly URL to the console where you have
+// to copy and paste it like an animal. Only tested on macOS. Use and enjoy but
+// maybe don't commit any call sites.
+//
+// Add `import { LaunchPlayground } from 'common/helpers/LaunchPlayground';`
+// to the top of your file or test and call like so:
+// `LaunchPlayground(window.document.body);`
+// element: a chunk of the DOM or a query result from the `screen` object in the
+// testing harness.
+//
+// returns happiness
+export const LaunchPlayground = (element: any): void => {
+  const unindented = element.innerHTML.replace(/[ \t]*[\n][ \t]*/g, '\n');
+  const encoded = compressToEncodedURIComponent(unindented);
+  execSync(`open https://testing-playground.com/#markup=${encoded}`);
+};
+
+export default LaunchPlayground;
--- src/components/overlays/Tooltip/Tooltip.test.tsx
+++ src/components/overlays/Tooltip/Tooltip.test.tsx
@@ -1,9 +1,71 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
-import { Tooltip } from './Tooltip';
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { Tooltip, TooltipProps } from './Tooltip';
+
+const mockOnShow = jest.fn();
+const mockOnHide = jest.fn();
+
+const contentText = 'Tooltip content';
+const content = <>{contentText}</>;
+const triggerText = 'Tooltip trigger';
+const trigger = <div>{triggerText}</div>;
+
+const renderTooltip = (props?: TooltipProps) => {
+    return render(<>
+		<Tooltip position="right" content={content} showDelay={0} hideDelay={0} {...props}>
+			{trigger}
+		</Tooltip>
+	</>);
+};
 
 describe('Tooltip', () => {
-	it('renders without crashing', () => {
-		shallow(<Tooltip />);
-	});
+	afterEach(() => {
+        jest.resetAllMocks();
+    });
+
+    test('renders correctly', () => {
+        renderTooltip();
+        expect(screen.getByText(triggerText)).toBeInTheDocument();
+    });
+
+	test('renders popper correctly on hover', async () => {
+        renderTooltip({ onHide: mockOnHide, onShow: mockOnShow });
+
+		userEvent.hover(screen.getByText(triggerText));
+		expect(await screen.findByText(contentText)).toBeInTheDocument();
+		expect(mockOnShow).toHaveBeenCalledTimes(1);
+		expect(mockOnHide).not.toHaveBeenCalled();
+	
+		userEvent.unhover(screen.getByText(triggerText));
+		expect(screen.queryByText(contentText)).not.toBeInTheDocument();
+		expect(mockOnShow).toHaveBeenCalledTimes(1);
+		expect(mockOnHide).toHaveBeenCalledTimes(1);
+    });
+
+	test('renders popper correctly immediately when forceShow is true', async () => {
+        renderTooltip({ forceShow: true });
+        expect(await screen.findByText(contentText)).toBeInTheDocument();
+    });
+
+	test('doesn\'t render popper when hideTooltip is true', () => {
+        const { unmount } = renderTooltip({ hideTooltip: true });
+		userEvent.hover(screen.getByText(triggerText));
+        expect(screen.queryByText(contentText)).not.toBeInTheDocument();
+		unmount();
+
+		renderTooltip({ hideTooltip: true, forceShow: true});
+		userEvent.hover(screen.getByText(triggerText));
+        expect(screen.queryByText(contentText)).not.toBeInTheDocument(); 
+    });
+
+	test('shows the tooltip on click instead of hover when useClickInsteadOfHover is true', async () => {
+        renderTooltip({ useClickInsteadOfHover: true });
+		userEvent.hover(screen.getByText(triggerText));
+        expect(screen.queryByText(contentText)).not.toBeInTheDocument();
+		userEvent.unhover(screen.getByText(triggerText));
+
+		userEvent.click(screen.getByText(triggerText));
+		expect(await screen.findByText(contentText)).toBeInTheDocument();
+    });
 });
