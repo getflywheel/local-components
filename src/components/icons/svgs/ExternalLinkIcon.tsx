b37a92328dc6fcd593b1b71ec4ebc097fb0253c9
Adam Perry <adam.perry@wpengine.com>
2022-03-11 14:44:53 -0600

    feat: add external link icon

---
+++ src/components/icons/svgs/ExternalLinkIcon.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ExternalLink from '../../../svg/external-link.svg';
+import withIconSvg from '../helpers/withIconSvg';
+
+export const ExternalLinkIcon: React.FC = ({...props}) => {
+	return (
+		<ExternalLink {...props} />
+	);
+};
+
+export default withIconSvg(ExternalLinkIcon, true, { tags: ['external', 'browser', 'open', 'link']});
