15a691af2757f3db5125d2ad3dea50cdccfa16b6
Adam Perry <adam.perry@wpengine.com>
2022-04-08 13:32:50 -0500

    feat: update Stepper component and grow steps

--- src/components/menus/Stepper/Stepper.tsx
+++ src/components/menus/Stepper/Stepper.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import IReactComponentProps from '../../../common/structures/IReactComponentProps';
 import classnames from 'classnames';
+import IReactComponentProps from '../../../common/structures/IReactComponentProps';
 import CompleteSVG from '../../../svg/complete.svg';
 import * as styles from './Stepper.sass';
 
@@ -8,56 +8,31 @@ interface IProps extends IReactComponentProps {
 	children: React.ReactNode;
 }
 
-export class Stepper extends React.Component<IProps> {
-	render () {
-		return (
-			<div
-				className={classnames(
-					styles.Stepper,
-					{
-						[styles.__Steps__2]: (this.props.children as React.ReactNode[]).length === 2,
-						[styles.__Steps__3]: (this.props.children as React.ReactNode[]).length === 3,
-					},
-					this.props.className,
-				)}
-				id={this.props.id}
-				style={this.props.style}
-			>
-				{this.props.children}
-			</div>
-		);
-	}
-}
+export const Stepper: React.FC<IProps> = ({ children, className, ...otherProps }: IProps) => (
+	<div className={classnames(styles.Stepper, className)} {...otherProps}>
+		{children}
+	</div>
+);
 
-interface IStepProps extends IReactComponentProps {
+interface IStepProps {
+	children?: React.ReactNode;
 	active?: boolean;
 	disabled?: boolean;
 	done: boolean;
 	number: number;
 }
 
-export class Step extends React.Component<IStepProps> {
-	render () {
-		return (
-			<div
-				className={classnames(
-					styles.Step,
-					{
-						[styles.Step__Done]: this.props.done,
-						[styles.Step__Active]: this.props.active,
-						[styles.Step__Disabled]: this.props.disabled,
-					},
-				)}
-			>
-				{
-					!this.props.done
-						?
-						<span>{this.props.number}</span>
-						:
-						<CompleteSVG />
-				}
-				{this.props.children}
-			</div>
-		);
-	}
-}
+export const Step: React.FC<IStepProps> = ({ done, active, disabled, number, children }: IStepProps) => {
+	return (
+		<div
+			className={classnames(styles.Step, {
+				[styles.Step__Done]: done,
+				[styles.Step__Active]: active,
+				[styles.Step__Disabled]: disabled,
+			})}
+		>
+			{!done ? <span>{number}</span> : <CompleteSVG />}
+			{children}
+		</div>
+	);
+};
