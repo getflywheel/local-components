37b1e0a80245251badc74005fb5b1a1d92af322a
Benjamin Turner <benjamin@passionsplay.com>
2022-04-21 14:53:31 -0700

    feat: Update storybook config
    - Make Storybook watch for `*.stories.jsx` files so we get the full
      power of JS when working with these Stories
    
    - Enable the `actions` addon so that we get automated logging of
      actions in the UI. See: https://storybook.js.org/docs/react/essentials/actions

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,38 +3,39 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    "stories": [
-        "../src/**/*.stories.mdx",
-    ],
-    "addons": [
-        'storybook-addon-themes',
-        {
-            name: '@storybook/addon-essentials',
-            options: {
-                viewport: false,
-                backgrounds: false,
-                actions: false,
-                controls: false,
-            }
-        }
-    ],
-    webpackFinal: (config) => {
-        const fileLoaderRule = config.module.rules.find(rule => rule.test.test && rule.test.test('.svg'));
-        fileLoaderRule.exclude = /\.svg$/;
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.jsx'],
+	addons: [
+		'storybook-addon-themes',
+		'@storybook/addon-actions',
+		{
+			name: '@storybook/addon-essentials',
+			options: {
+				viewport: false,
+				backgrounds: false,
+				actions: false,
+				controls: false,
+			},
+		},
+	],
+	webpackFinal: (config) => {
+		const fileLoaderRule = config.module.rules.find((rule) => rule.test.test && rule.test.test('.svg'));
+		fileLoaderRule.exclude = /\.svg$/;
 
-        config.module.rules.unshift(sharedRules.sass({
-            loader: 'style-loader'
-        }));
+		config.module.rules.unshift(
+			sharedRules.sass({
+				loader: 'style-loader',
+			})
+		);
 
-        config.module.rules.unshift(sharedRules.reactSvg);
-        config.module.rules.unshift(sharedRules.cssSvg);
+		config.module.rules.unshift(sharedRules.reactSvg);
+		config.module.rules.unshift(sharedRules.cssSvg);
 
-        config.plugins.push(
-            new webpack.NormalModuleReplacementPlugin(/electron/, function (resource) {
-                resource.request = path.resolve(__dirname, '../src', '__mocks__', 'electron.js');
-            })
-        );
+		config.plugins.push(
+			new webpack.NormalModuleReplacementPlugin(/electron/, function (resource) {
+				resource.request = path.resolve(__dirname, '../src', '__mocks__', 'electron.js');
+			})
+		);
 
-        return config;
-    },
-}
+		return config;
+	},
+};
