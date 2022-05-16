2547114fce51cb22d17994ba43bf7ff2d7bf382a
Adam Perry <adam.perry@wpengine.com>
2022-05-16 16:28:54 -0500

    chore: upgrade webpack dev server from v3 to v4

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const webpack = require('webpack');
 const merge = require('webpack-merge');
-const WriteFilePlugin = require('write-file-webpack-plugin');
 
 const port = process.env.PORT || 1212;
 const publicPath = `http://localhost:${port}/dist`;
@@ -10,21 +8,18 @@ module.exports = merge.strategy({
 	entry: 'prepend',
 })(require('./webpack.common.js'), {
 	mode: 'development',
-	entry: [`webpack-dev-server/client?http://localhost:${port}/`, 'webpack/hot/only-dev-server'],
+	entry: [`webpack-dev-server/client?http://localhost:${port}/`],
 	output: {
 		publicPath: `http://localhost:${port}/dist/`,
 	},
 	devServer: {
 		port,
-		publicPath,
-		hot: true,
+		hot: 'only',
 		headers: { 'Access-Control-Allow-Origin': '*' },
-		contentBase: path.join(__dirname, 'dist'),
-		watchOptions: {
-			aggregateTimeout: 300,
-			ignored: /node_modules/,
-			poll: 100,
+		devMiddleware: {
+			publicPath,
+			writeToDisk: true,
 		},
+		static: path.join(__dirname, 'dist'),
 	},
-	plugins: [new WriteFilePlugin(), new webpack.HotModuleReplacementPlugin()],
 });
