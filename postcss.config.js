db8008db2e18ede045b1f1d01370f97a20ba5de3
Adam Perry <adam.perry@wpengine.com>
2022-05-18 14:27:10 -0500

    feat: various postcss related updates and cleanup

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,4 @@
-const path = require('path');
-
 module.exports = {
-	plugins: [
-		require('postcss-modules'),
-		require('postcss-url')({
-			url: 'inline',
-			basePath: path.resolve('./src/styles'),
-			optimizeSvgEncode: true,
-		})
-	]
+	// eslint-disable-next-line global-require
+	plugins: [require('postcss-modules')],
 };
