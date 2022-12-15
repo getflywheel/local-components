1a97eca71204fa5ebdedc27e9524395595b66698
Adam Perry <adam.perry@wpengine.com>
2022-12-15 14:32:01 -0600

    fix: move toast styles to top of head

--- src/components/alerts/ToastContainer/ToastContainer.tsx
+++ src/components/alerts/ToastContainer/ToastContainer.tsx
@@ -2,7 +2,28 @@ import React from 'react';
 import { toast as toastDefault, ToastContainer as ToastContainerDefault, ToastContainerProps } from 'react-toastify';
 import { injectStyle } from 'react-toastify/dist/inject-style';
 
-injectStyle();
+const injectToastStyles = () => {
+	injectStyle();
+	let toastStyleSheet: CSSStyleSheet | undefined;
+	// eslint-disable-next-line no-restricted-syntax
+	for (const sheet of document.styleSheets) {
+		try {
+			if (sheet.cssRules?.[0].cssText.includes(':root') && sheet.cssRules[1]?.cssText.includes('.Toastify')) {
+				toastStyleSheet = sheet;
+				break;
+			}
+		} catch {
+			// eslint-disable-next-line no-continue
+			continue;
+		}
+	}
+
+	const node = toastStyleSheet?.ownerNode;
+	node?.remove();
+	document.head.insertBefore(node as Node, document.head.firstChild);
+};
+
+injectToastStyles();
 
 const ToastContainer = (props: ToastContainerProps) => {
 	const defaultProps: ToastContainerProps = {
