4353c06fdf0f24c8ecc2e75b949536d0640f38b4
Adam Perry <adam.perry@wpengine.com>
2023-02-20 15:22:53 -0600

    feat: add highlightText option to BasicInput
    Also exports the BasicInput props and adds an onKeyDown event to IReactComponentProps

--- src/components/inputs/BasicInput/BasicInput.tsx
+++ src/components/inputs/BasicInput/BasicInput.tsx
@@ -16,21 +16,27 @@ export interface IBasicInputProps extends ILocalContainerProps {
 	readonly?: boolean;
 	size?: number;
 	spellcheck?: boolean;
-	onKeyUp?: any;
 	invalid?: boolean;
 	invalidMessage?: string;
 	autofocus?: boolean;
 	disabled?: boolean;
+	/** Highlight the text on mount, useful for renaming */
+	highlightText?: boolean;
 }
 
 const BasicInput = React.forwardRef((props: IBasicInputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
-	const { className, id, name, style, invalid, invalidMessage, autofocus, disabled, ...otherProps } = props;
+	const { className, id, name, style, invalid, invalidMessage, autofocus, disabled, highlightText, ...otherProps } =
+		props;
 
 	const input = useForwardedRef(ref);
 
 	React.useEffect(() => {
 		if (autofocus) {
 			input.current?.focus();
+		}
+
+		if (highlightText) {
+			input.current?.select();
 		}
 	}, []);
 
