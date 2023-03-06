ed33b1c858c7fe3244bda573018b3e2354487359
Adam Perry <adam.perry@wpengine.com>
2023-03-06 16:43:38 -0600

    feat: handle submenus in ContextMenu, along with keyboard nav
    Recursively renders submenu items in a new renderItem helper.
    
    The ContextMenuTextButton helper component helps us render the buttons.
    
    Styles for checkmark items were added.
    
    Keyboard accessiblity was prioritized, with arrow keys primarily used. Helper functions for focusing the
    previous and next items were added. Expected behaviour comes from WCAG and system context menus.

--- src/components/menus/ContextMenu/ContextMenu.tsx
+++ src/components/menus/ContextMenu/ContextMenu.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import classnames from 'classnames';
 import shortid from 'shortid';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './ContextMenu.scss';
 import { FunctionGeneric } from '../../../common/structures/Generics';
-import { Tooltip, TooltipProps, hideTooltip } from '../../overlays/Tooltip/Tooltip';
-import { TextButton } from '../../buttons/TextButton/TextButton';
+import { Tooltip, TooltipProps, hideTooltip, showTooltip } from '../../overlays/Tooltip/Tooltip';
+import { ITextButtonProps, TextButton } from '../../buttons/TextButton/TextButton';
 import { ThreeDotButton } from '../../buttons/ThreeDotButton/ThreeDotButton';
+import { ArrowRightIcon, CheckmarkIcon } from '../../icons/Icons';
 
 export interface IMenuItem {
 	color?: 'red' | 'none';
@@ -15,10 +16,12 @@ export interface IMenuItem {
 	label?: string;
 	onClick?: FunctionGeneric;
 	enabled?: boolean;
-	type?: 'normal' | 'separator';
+	type?: 'normal' | 'separator' | 'checkbox';
+	submenu?: IMenuItem[];
+	checked?: boolean;
 }
 
-export interface IContextMenuProps extends Omit<TooltipProps, 'content' | 'useClickInsteadOfHover'> {
+export interface IContextMenuProps extends Omit<TooltipProps, 'content' | 'useClickInsteadOfHover' | 'ref'> {
 	/** className for the list items' container */
 	classNameList?: string;
 	/** className for an individual list item */
@@ -27,52 +30,232 @@ export interface IContextMenuProps extends Omit<TooltipProps, 'content' | 'useCl
 	items: IMenuItem[];
 	/** whether to have background behind 3 dots - set to false for low key 3 dot dropdowns */
 	noBG?: boolean;
-	/** whether to have background behind 3 dots - set to false for low key 3 dot dropdowns */
+	/** whether to have background behind 3 dots on hover only */
 	bgOnHover?: boolean;
+	/** Private property used to render submenus recursively, should be no need to set this */
+	isSubmenu?: boolean;
+	/** Private property used to render submenus recursively, should be no need to set this */
+	parentMenuId?: string;
 }
 
+interface ContextMenuTextButtonProps extends ITextButtonProps {
+	item: IMenuItem;
+	menuToClose: string;
+}
+
+const ContextMenuTextButton = ({ item, menuToClose, ...restProps }: ContextMenuTextButtonProps) => {
+	const onClickItem = (event: React.MouseEvent<HTMLElement, MouseEvent>, menuItem: IMenuItem) => {
+		event.preventDefault();
+		event.stopPropagation();
+		if (menuItem.onClick) {
+			menuItem.onClick.call(null);
+			hideTooltip(menuToClose);
+		}
+	};
+
+	return (
+		<TextButton
+			className={classnames(styles.ContextMenu_Item_TextButton, {
+				[styles.ContextMenu_Item_TextButton__Red]: item.color === 'red',
+				[styles.ContextMenu_Item_TextButton__Checkbox]: item.type === 'checkbox',
+				[styles.ContextMenu_Item_TextButton__Checked]: item.checked && item.type === 'checkbox',
+				[styles.ContextMenu_Submenu_Item_TextButton]: !!item.submenu,
+			})}
+			disabled={item.enabled === undefined ? false : !item.enabled}
+			intent={item.color === 'red' ? 'destructive' : 'default'}
+			onClick={(event) => !item.submenu && onClickItem(event, item)}
+			tabIndex={-1}
+			{...restProps}
+		>
+			{item.label}
+			{item.content}
+		</TextButton>
+	);
+};
+
 const ContextMenu = (props: IContextMenuProps) => {
-	const { className, classNameList, classNameListItem, items, onShow, onHide, noBG, bgOnHover, id, ...otherProps } =
-		props;
+	const {
+		className,
+		classNameList,
+		classNameListItem,
+		items,
+		onShow,
+		onHide,
+		noBG,
+		bgOnHover,
+		id,
+		isSubmenu,
+		children,
+		onKeyDown,
+		parentMenuId,
+		...otherProps
+	} = props;
 
 	const menuId = useRef(id ?? `${'contextMenu'}-${shortid.generate()}`);
 
-	const [isShowing, setIsShowing] = React.useState(false);
+	const [isShowing, setIsShowing] = useState(false);
 
-	const onClickItem = (event: React.MouseEvent<HTMLLIElement, MouseEvent>, item: IMenuItem) => {
-		event.preventDefault();
-		event.stopPropagation();
-		if (item.onClick) {
-			item.onClick.call(null);
-			hideTooltip(menuId.current);
+	const triggerRef = useRef<HTMLElement | null>(null);
+	const contextMenuRef = useRef<HTMLElement | null>(null);
+
+	const [, setFocusedItemIndex] = useState(-1);
+
+	const enabledItemsWithRefs = useRef<Array<(IMenuItem & { ref: HTMLElement }) | undefined>>([]);
+
+	const renderItem = (item: IMenuItem, index: number) => {
+		const maybeSetItemRef = (el: HTMLElement) => {
+			enabledItemsWithRefs.current[index] = item.enabled === false ? undefined : { ...item, ref: el };
+		};
+
+		const parentMenu = parentMenuId ?? menuId.current;
+
+		if (item.submenu) {
+			return (
+				<ContextMenu
+					items={item.submenu}
+					position="right-start"
+					className={styles.ContextMenu__Submenu}
+					popperOffsetModifier={{ offset: [-15, 15] }}
+					stopPopperClickPropagation
+					id={`${menuId.current}-submenu${index}`}
+					isSubmenu
+					parentMenuId={parentMenu}
+				>
+					<ContextMenuTextButton
+						item={item}
+						rightIcon={ArrowRightIcon}
+						menuToClose={parentMenu}
+						innerRef={maybeSetItemRef}
+						aria-label="Open context menu submenu"
+						aria-haspopup
+					/>
+				</ContextMenu>
+			);
+		}
+
+		switch (item.type) {
+			case 'separator':
+				return <div className={styles.ContextMenu_Divider} />;
+			case 'checkbox':
+				return (
+					<ContextMenuTextButton
+						item={item}
+						menuToClose={parentMenu}
+						leftIcon={item.checked && CheckmarkIcon}
+						innerRef={maybeSetItemRef}
+					/>
+				);
+			default:
+				return <ContextMenuTextButton item={item} menuToClose={parentMenu} innerRef={maybeSetItemRef} />;
 		}
 	};
 
 	const content = (
-		<ul className={classnames(styles.ContextMenu_Items, 'ContextMenu_Items', classNameList)}>
+		<ul className={classnames(styles.ContextMenu_Items, 'ContextMenu_Items', classNameList)} role="menu">
 			{items
 				.filter((item: IMenuItem) => Object.keys(item).length !== 0)
 				.map((item: IMenuItem, i: number) => (
-					// eslint-disable-next-line react/no-array-index-key
-					<li key={i} className={classnames(styles.ContextMenu_Item, classNameListItem, item.className)}>
-						{item.type === 'separator' ? (
-							<div className={styles.ContextMenu_Divider} />
-						) : (
-							<TextButton
-								className={item.color !== 'red' ? styles.ContextMenu_Item_TextButton : ''}
-								onClick={(event) => onClickItem(event, item)}
-								disabled={item.enabled === undefined ? false : !item.enabled}
-								intent={item.color === 'red' ? 'destructive' : 'default'}
-								style={{ padding: '5px 0' }}
-							>
-								{item.label}
-								{item.content}
-							</TextButton>
-						)}
+					<li
+						// eslint-disable-next-line react/no-array-index-key
+						key={`${menuId}-${i}`}
+						className={classnames(styles.ContextMenu_Item, classNameListItem, item.className)}
+						role={item.type !== 'separator' ? 'menuitem' : ''}
+					>
+						{renderItem(item, i)}
 					</li>
 				))}
 		</ul>
 	);
+
+	const handleEscapeOrTab = (e: KeyboardEvent) => {
+		if (e.key === 'Escape' || e.key === 'Tab') {
+			triggerRef.current?.focus();
+			hideTooltip(menuId.current);
+		}
+	};
+
+	useEffect(() => {
+		if (isShowing) {
+			document.addEventListener('keydown', handleEscapeOrTab);
+		}
+
+		return () => document.removeEventListener('keydown', handleEscapeOrTab);
+	}, [isShowing]);
+
+	const focusNextItem = () => {
+		setFocusedItemIndex((prev) => {
+			const arr = enabledItemsWithRefs.current;
+			for (let i = prev + 1; i < arr.length; i += 1) {
+				const item = arr[i];
+				if (item && item.enabled !== false) {
+					// The 1ms timeout handles focusing a submenu's first item after the 1ms transition open
+					setTimeout(() => item.ref.focus(), 1);
+					return i;
+				}
+			}
+
+			return prev;
+		});
+	};
+
+	const focusPreviousItem = () => {
+		setFocusedItemIndex((prev) => {
+			const arr = enabledItemsWithRefs.current;
+			for (let i = prev - 1; i >= 0; i -= 1) {
+				const item = arr[i];
+				if (item && item.enabled !== false) {
+					item.ref.focus();
+					return i;
+				}
+			}
+
+			return prev;
+		});
+	};
+
+	const handleKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
+		e.persist();
+		onKeyDown?.(e);
+
+		switch (e.key) {
+			case 'ArrowDown':
+				if (isShowing) {
+					document.body.classList.remove('using-mouse');
+					focusNextItem();
+
+					if (isSubmenu) {
+						e.stopPropagation();
+					}
+				}
+				break;
+			case 'ArrowUp':
+				if (isShowing) {
+					document.body.classList.remove('using-mouse');
+					focusPreviousItem();
+
+					if (isSubmenu) {
+						e.stopPropagation();
+					}
+				}
+				break;
+			case 'ArrowRight':
+				if (isSubmenu) {
+					e.stopPropagation();
+					showTooltip(menuId.current);
+				}
+				break;
+			case 'ArrowLeft':
+				if (isSubmenu && !e.currentTarget.contains(e.target as Node)) {
+					e.stopPropagation();
+					hideTooltip(menuId.current);
+					(contextMenuRef.current?.firstChild as HTMLElement)?.focus();
+				}
+
+				break;
+
+			default:
+		}
+	};
 
 	return (
 		<Tooltip
@@ -81,23 +264,43 @@ const ContextMenu = (props: IContextMenuProps) => {
 			onShow={() => {
 				onShow?.call(null);
 				setIsShowing(true);
+				if (!isSubmenu) {
+					triggerRef.current?.focus();
+				} else {
+					focusNextItem();
+				}
 			}}
 			onHide={() => {
 				onHide?.call(null);
 				setIsShowing(false);
+				setFocusedItemIndex(-1);
 			}}
-			focusOnOpen
 			useClickInsteadOfHover
 			id={menuId.current}
-			{...otherProps}
+			onKeyDown={handleKeyDown}
 			content={content}
+			ref={contextMenuRef}
+			{...otherProps}
 		>
-			<ThreeDotButton aria-label="Open context menu" active={isShowing} noBG={noBG} bgOnHover={bgOnHover} />
+			{children ?? (
+				<ThreeDotButton
+					data-testid="ThreeDotButton"
+					innerRef={(el) => {
+						triggerRef.current = el;
+					}}
+					aria-label="Open context menu"
+					active={isShowing}
+					noBG={noBG}
+					bgOnHover={bgOnHover}
+					aria-haspopup
+				/>
+			)}
 		</Tooltip>
 	);
 };
 
 ContextMenu.defaultProps = {
+	noAnimations: true,
 	items: [],
 	position: 'bottom-start',
 	hideArrow: true,
